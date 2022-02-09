5109b0c72b4d49da28dd5376054de114b03afea8
BartaBalazs00 <77434768+BartaBalazs00@users.noreply.github.com>
2022-02-09 09:04:53 +0100

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-sm-3">
+            <img src="/img/FilmEst.jpg" class="rounded-circle p-5 h-50">
+        </div>
+        <div class="col-lg-9 pt-5">
+            <div><h1>Film Est</h1></div>
+            <div class="d-flex">
+                <div class="pe-5"><strong>153</strong> saved films</div>
+                <div class="pe-5"><strong>23k</strong> Followers</div>
+                <div class="pe-5"><strong>212</strong> Following</div>
+            </div>
+        </div>
+        </div>
+    </div>
+</div>
+@endsection

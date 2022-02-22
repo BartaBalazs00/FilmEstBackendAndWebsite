db67c800674c489bf4431496e6af180d7199882f
Schmidt Gergő <s.gergo.az@gmail.com>
2022-02-22 11:54:14 +0100

    Apu routes

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,9 @@
 <?php
 
 use App\Http\Controllers\Api\FilmController;
+use App\Http\Controllers\Api\KategoriaController;
+use App\Http\Controllers\Api\RendezoController;
+use App\Http\Controllers\Api\SzineszController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -19,3 +22,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::resource('/filmek', FilmController::class);
+Route::resource('/kategoriak', KategoriaController::class);
+Route::resource('/rendezok', RendezoController::class);
+Route::resource('/szineszek', SzineszController::class);

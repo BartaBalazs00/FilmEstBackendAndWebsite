fe0881b149fa15011db02cb8fb3cb5bb2ff123c4
Barta Balázs <barta.balazs00@gmail.com>
2022-03-07 10:02:44 +0100

    follow gomb hozzáadása

--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -7,13 +7,16 @@
             <img src="{{ $user->profile->profileImage()}}" class=" rounded-circle mt-5 height: auto img-fluid">
         </div>
             <div class="col-lg-9 col-sm-6 pt-5">
-                <div><h1>{{$user->username}}</h1></div>
-
+                <div class="d-flex align-items-center pb-3 ">
+                    <div class="h4">{{$user->username}}</div>
+                    
+                    <button class="btn btn-primary ms-4">Follow</button>
+                </div>
                 @can('update', $user->profile)
                     <div class="pb-3"><a href="/profile/{{$user->id}}/edit">Profil szerkesztése</a></div>
                 @endcan
                 
-                <div class="pe-5"><strong>{{$mentettFilmek->count()}}</strong> mentett filmek</div>
+                <div class="pe-5"><strong>{{$mentettFilmek->count()}}</strong> mentett film</div>
                 <div class="pt-3"><strong>{{$user->profile->cim}}</strong></div>
                 <div class="pt-3">{{$user->profile->leiras}}</div>
                 <div class="pt-3 fw-bold"><a href="#"> {{$user->profile->url}}</a></div>

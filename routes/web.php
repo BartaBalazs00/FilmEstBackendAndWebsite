091bfe4cffdca01fb2453ff5464f74761bea9f17
BartaBalazs00 <77434768+BartaBalazs00@users.noreply.github.com>
2022-03-13 22:37:34 +0100

    email verification

--- routes/web.php
+++ routes/web.php
@@ -24,10 +24,10 @@ Route::get('/profile', [App\Http\Controllers\ProfilesController::class, 'index']
 Route::get('/profile/search-user', [App\Http\Controllers\ProfilesController::class, "search"])->name("user.search");
 Route::get('/profile/{user}', [App\Http\Controllers\ProfilesController::class, 'show'])->name('profile.show');
 
-Route::get('/profile/{user}/following', [App\Http\Controllers\ProfilesController::class, 'following'])->name('profile.following');
-Route::get('/profile/{user}/followers', [App\Http\Controllers\ProfilesController::class, 'followers'])->name('profile.followers');
-Route::get('/profile/{user}/edit', [App\Http\Controllers\ProfilesController::class, 'edit'])->name('profile.edit');
-Route::patch('/profile/{user}', [App\Http\Controllers\ProfilesController::class, 'update'])->name('profile.update');
+Route::get('/profile/{user}/following', [App\Http\Controllers\ProfilesController::class, 'following'])->middleware('verified')->name('profile.following');
+Route::get('/profile/{user}/followers', [App\Http\Controllers\ProfilesController::class, 'followers'])->middleware('verified')->name('profile.followers');
+Route::get('/profile/{user}/edit', [App\Http\Controllers\ProfilesController::class, 'edit'])->middleware('verified')->name('profile.edit');
+Route::patch('/profile/{user}', [App\Http\Controllers\ProfilesController::class, 'update'])->middleware('verified')->name('profile.update');
 
 //Route::get('/film/{filmId}', [App\Http\Controllers\FilmController::class, 'show'])->name('film.show');
 //Route::post('/film/{filmId}/store', [App\Http\Controllers\FilmController::class, 'save'])->name('film.store');
@@ -38,4 +38,4 @@ Route::resource('film', FilmController::class);
 //Route::post('/search-film', [App\Http\Controllers\FilmController::class, "search"])->name("search");
 Route::get('/search-film', [App\Http\Controllers\FilmController::class, "search"])->name("film.search");
 
-Auth::routes();
+Auth::routes(['verify' => true]);
